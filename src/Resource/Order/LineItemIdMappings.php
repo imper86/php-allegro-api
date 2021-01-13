6b6031d3de787222440138cbde6682ab1ceeec24
imper <me@imper.info>
2021-01-13 10:19:00 +0100

    deprecated GET /order/line-item-id-mappings https://github.com/allegro/allegro-api/issues/4028

--- src/Resource/Order/LineItemIdMappings.php
+++ src/Resource/Order/LineItemIdMappings.php
@@ -7,8 +7,18 @@ namespace Imper86\PhpAllegroApi\Resource\Order;
 use Imper86\PhpAllegroApi\Resource\AbstractResource;
 use Psr\Http\Message\ResponseInterface;
 
+/**
+ * Class LineItemIdMappings
+ * @package Imper86\PhpAllegroApi\Resource\Order
+ * @deprecated
+ */
 class LineItemIdMappings extends AbstractResource
 {
+    /**
+     * @param array $query
+     * @return ResponseInterface
+     * @deprecated
+     */
     public function get(array $query): ResponseInterface
     {
         return $this->apiGet('/order/line-item-id-mappings', $query);
