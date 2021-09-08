824acb21ae0a6c2a8aaca80124fa0c7f725bb28b
imper <me@imper.info>
2021-09-08 12:45:36 +0200

    added beta v3 for product-offers https://github.com/allegro/allegro-api/issues/4711

--- src/Enum/ContentType.php
+++ src/Enum/ContentType.php
@@ -12,4 +12,5 @@ final class ContentType
     public const VND_PUBLIC_V2 = 'application/vnd.allegro.public.v2+json';
     public const VND_BETA_V1 = 'application/vnd.allegro.beta.v1+json';
     public const VND_BETA_V2 = 'application/vnd.allegro.beta.v2+json';
+    public const VND_BETA_V3 = 'application/vnd.allegro.beta.v3+json';
 }
