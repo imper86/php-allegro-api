3256a1b1d91f159321d29fa08c472271dfd6db1b
imper <me@imper.info>
2021-09-08 13:32:01 +0200

    marked FeePreview.php as deprecated: https://github.com/allegro/allegro-api/issues/4728

--- src/Resource/Pricing/FeePreview.php
+++ src/Resource/Pricing/FeePreview.php
@@ -7,11 +7,15 @@ namespace Imper86\PhpAllegroApi\Resource\Pricing;
 use Imper86\PhpAllegroApi\Resource\AbstractResource;
 use Psr\Http\Message\ResponseInterface;
 
+/**
+ * @deprecated
+ */
 class FeePreview extends AbstractResource
 {
     /**
      * @param mixed[] $body
      * @return ResponseInterface
+     * @deprecated
      */
     public function post(array $body): ResponseInterface
     {
