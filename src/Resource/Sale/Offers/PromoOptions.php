ce5a80f1b48238614324253796952d7cbf37ff9b
devEcommercePL <155567765+devEcommercePL@users.noreply.github.com>
2025-12-07 22:39:04 +0100

    Make `PromoOptions::get` method parameters optional and support query parameters. (#24)
    Co-authored-by: Lukas <lukas@e-projects.pl>

--- src/Resource/Sale/Offers/PromoOptions.php
+++ src/Resource/Sale/Offers/PromoOptions.php
@@ -7,8 +7,8 @@ use Psr\Http\Message\ResponseInterface;
 
 class PromoOptions extends AbstractResource
 {
-    public function get(string $offerId): ResponseInterface
+    public function get(?string $offerId = null, ?array $query = null): ResponseInterface
     {
-        return $this->apiGet(sprintf('/sale/offers/%s/promo-options', $offerId));
+        return $this->apiGet(sprintf('/sale/offers/%s', $offerId ? "{$offerId}/" : '') . 'promo-options', $query);
     }
 }
